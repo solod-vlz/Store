a2be9301a0160c110542e39e613cf4173b5d0287
Andrey <solod-vlz@mail.ru>
2022-08-02 17:32:22 +0300

    Remove trash

--- presentation/Store.Web/Controllers/HomeController.cs
+++ presentation/Store.Web/Controllers/HomeController.cs
@@ -11,19 +11,7 @@ namespace Store.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
