2fa9cad68c6788d1dfbc3b1de116cf712a846c16
Andrey <solod-vlz@mail.ru>
2022-08-04 13:17:38 +0300

    Add Order class and tests for it

---
+++ domain/Store.Tests/OrderTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Store.Tests
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void Order_WithNullItems_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Order(1, null));
+        }
+
+        [Fact]
+        public void TotalCount_WithEmptyItems_ReturnZero()
+        {
+            var order = new Order(1, new List<OrderItem>() { });
+
+            Assert.Equal(0, order.TotalCount);
+        }
+
+        [Fact]
+        public void TotalPrice_WithEmptyItems_ReturnZero()
+        {
+            var order = new Order(1, new List<OrderItem>() { });
+
+            Assert.Equal(0m, order.TotalPrice);
+        }
+
+        [Fact]
+        public void TotalCount_WithNoEmptyItems_CalculatesTotalCount()
+        {
+            var order = new Order(1, new List<OrderItem>() 
+            { 
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m)
+            });
+
+            Assert.Equal(3+5, order.TotalCount);
+        }
+
+        [Fact]
+        public void TotalPrice_WithNoEmptyItems_CalculatesTotalPrice()
+        {
+            var order = new Order(1, new List<OrderItem>()
+            {
+                new OrderItem(1, 3, 10m),
+                new OrderItem(2, 5, 100m)
+            });
+
+            Assert.Equal(3*10m + 5*100m, order.TotalPrice);
+        }
+    }
+}
---
+++ domain/Store/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store
+{
+    public class Order
+    {
+        public int Id { get; }
+
+        private List<OrderItem> items;
+
+        public IReadOnlyCollection<OrderItem> Items
+        {
+            get => items;
+        }
+        
+        public Order(int id, IEnumerable<OrderItem> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            Id = id;
+            this.items = new List<OrderItem>(items);
+        }
+
+        public int TotalCount
+        {
+            get => items.Sum(item => item.Count);
+        }
+
+        public decimal TotalPrice
+        {
+            get => items.Sum(item => item.Count * item.Price);
+        }
+    }
+}
