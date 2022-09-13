148a4699751c923e1c670658960f0279360ad932
Andrey <solod-vlz@mail.ru>
2022-09-13 21:41:35 +0300

    Add db interraction

--- infrastructure/Store.Data.EF/ServiceCollectionExtensions.cs
+++ infrastructure/Store.Data.EF/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 
 namespace Store.Data.EF
 {
@@ -13,7 +15,9 @@ namespace Store.Data.EF
                     options.UseSqlServer(connectionString);
                 },
                 ServiceLifetime.Transient);
-            
+
+            services.AddScoped<Dictionary<Type, StoreDbContext>>();
+            services.AddSingleton<DbContextFactory>();
             services.AddSingleton<IBookRepository, BookRepository>();
             services.AddSingleton<IOrderRepository, OrderRepository>();
 
